0585bc0e4c0f853394444ad908f195a7cc036951
vladsuxunun <vladsuxunun@gmail.com>
2022-01-03 14:45:37 +0600

    ddd

--- utos.py
+++ utos.py
@@ -8,17 +8,22 @@ import os
 def ssphoto(tokens):
    succs = "-"
    print("dd")
+   randstr = random.randint(36,51)
+
    if True:
        vk_session = vk_api.VkApi(token = tokens)
        time.sleep(1)
        vk = vk_session.get_api()
        vk.account.getProfileInfo()
+       if randstr >= 44 and randstr <= 51:
+          pathrand = str(44)
+       else:
+          pass
        basedir = os.path.abspath(os.path.dirname(__file__))
        cnt1 = 1
-       pathrand = str(1)
        time.sleep(4)
        upload = vk_api.VkUpload(vk_session)
-       photo = upload.photo_profile(photo = os.path.join(basedir, ('/app/ss'+ pathrand +'/' + str(random.randint(1,50))+ '.jpg')))
+       photo = upload.photo_profile(photo = os.path.join(basedir, ('/app/ssm'+ pathrand +'/' + str(random.randint(1,50))+ '.jpg')))
        time.sleep(3)
        album = vk.photos.createAlbum(title = 'фото')
        ss = album['id']
@@ -32,11 +37,12 @@ def ssphoto(tokens):
        time.sleep(1)
        try:
           upload = vk_api.VkUpload(vk_session)
-          upload.photo(photos=os.path.join(basedir, ('/app/ss' + pathrand +'/' + str(random.randint(1,50)) + '.jpg')),album_id =ss)
+          upload.photo(photos=os.path.join(basedir, ('/app/ssm' + pathrand +'/' + str(i) + '.jpg')),album_id =ss)
           upload = 1
        except:
           pass
    else:
       pass
+
 
    
